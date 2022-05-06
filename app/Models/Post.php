eda4bc36a68f7b08553a8fc8b745ae1dc76b172b
mgundogan33 <gundogan.mehmet33@gmail.com>
2022-05-06 11:57:13 +0300

    11

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,20 +4,25 @@ namespace App\Models;
 
 use App\Models\User;
 use App\Models\Category;
+use App\Models\Tag;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Post extends Model
 {
     use HasFactory;
-    protected $fillable=['title','slug','excerpt','body','user_id','category_id'];
+    protected $fillable = ['title', 'slug', 'excerpt', 'body', 'user_id', 'category_id'];
 
     public function author()
     {
-        return $this->belongsTo(User::class,'user_id');
+        return $this->belongsTo(User::class, 'user_id');
     }
     public function category()
     {
         return $this->belongsTo(Category::class);
+    }
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class);
     }
 }
