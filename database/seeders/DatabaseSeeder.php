a0b5a4dbe9b70614fd701c47afa5cc1c19189b85
mgundogan33 <gundogan.mehmet33@gmail.com>
2022-05-07 11:51:00 +0300

    15

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +15,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        Schema::disableForeignKeyConstraints();
+        \App\Models\User::truncate();
+        \App\Models\Role::truncate();
+         \App\Models\Category::truncate();
+        \App\Models\Post::truncate();
+        Schema::enableForeignKeyConstraints();
+
+         \App\Models\Role::factory(1)->create();
+         \App\Models\User::factory(10)->create();
+
+         \App\Models\Category::factory(10)->create();
+         \App\Models\Post::factory(100)->create();
+
     }
 }
