916788718c23a45fe2fc8396f150cfe33d6eab86
mgundogan33 <gundogan.mehmet33@gmail.com>
2022-05-11 15:25:12 +0300

    39

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,11 @@
 <?php
 
+
+
+use App\Http\Controllers\AdminControllers\DashboardController;
+
+
+
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TagController;
 use App\Http\Controllers\HomeController;
@@ -7,33 +13,30 @@ use App\Http\Controllers\AboutController;
 use App\Http\Controllers\PostsController;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\CategoryController;
+use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+// Front User Route
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
-Route::get('/posts/{post:slug}', [PostsController::class,'show'])->name('posts.show');
-Route::post('/posts/{post:slug}', [PostsController::class,'addComment'])->name('posts.add_comment');
+Route::get('/posts/{post:slug}', [PostsController::class, 'show'])->name('posts.show');
+Route::post('/posts/{post:slug}', [PostsController::class, 'addComment'])->name('posts.add_comment');
 
 
-Route::get('/about',AboutController::class)->name('about');
-Route::get('/contact',[ContactController::class,'create'])->name('contact.create');
-Route::post('/contact',[ContactController::class,'store'])->name('contact.store');
+Route::get('/about', AboutController::class)->name('about');
+Route::get('/contact', [ContactController::class, 'create'])->name('contact.create');
+Route::post('/contact', [ContactController::class, 'store'])->name('contact.store');
 
-Route::get('/categories/{category:slug}',[CategoryController::class,'show'])->name('categories.show');
-Route::get('/categories',[CategoryController::class,'index'])->name('categories.index');
+Route::get('/categories/{category:slug}', [CategoryController::class, 'show'])->name('categories.show');
+Route::get('/categories', [CategoryController::class, 'index'])->name('categories.index');
 
 
-Route::get('/tag/{tag:name}',[TagController::class,'show'])->name('tags.show');
+Route::get('/tag/{tag:name}', [TagController::class, 'show'])->name('tags.show');
 
 
 require __DIR__ . '/auth.php';
+
+
+// Admin Dashboard Routes
+
+Route::get('admin', [DashboardController::class, 'index'])->name('admin.index');
