fa8b2c971409a56c5d4bd1afd6d78a386e34f5cb
mgundogan33 <gundogan.mehmet33@gmail.com>
2022-05-16 16:13:15 +0300

    57

--- app/Http/Controllers/AdminControllers/AdminCommentsController.php
+++ app/Http/Controllers/AdminControllers/AdminCommentsController.php
@@ -3,11 +3,17 @@
 namespace App\Http\Controllers\AdminControllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Comment;
 use App\Models\Post;
 use Illuminate\Http\Request;
 
 class AdminCommentsController extends Controller
 {
+    private $rules = [
+        'post_id' => 'required|numeric',
+        'the_comment' => 'required|min:3|max:1000'
+    ];
+
     public function index()
     {
         //
@@ -15,14 +21,18 @@ class AdminCommentsController extends Controller
 
     public function create()
     {
-        return view('admin_dashboard.comments.create',[
-            'posts'=>Post::pluck('title','id')
+        return view('admin_dashboard.comments.create', [
+            'posts' => Post::pluck('title', 'id')
         ]);
     }
 
     public function store(Request $request)
     {
-        //
+        $validated = $request->validate($this->rules);
+        $validated['user_id'] = auth()->id();
+
+        Comment::create($validated);
+        return redirect()->route('admin.comments.create')->with('success', 'Comment has been added');
     }
 
     public function show($id)
@@ -32,14 +42,17 @@ class AdminCommentsController extends Controller
 
     public function edit($id)
     {
-        return view('admin_dashboard.comments.edit',[
-            'posts'=>Post::pluck('id','title')
+        return view('admin_dashboard.comments.edit', [
+            'posts' => Post::pluck('id', 'title')
         ]);
     }
 
-    public function update(Request $request, $id)
+    public function update(Request $request, Comment $comment)
     {
-        //
+
+        $validated = $request->validate($this->rules);
+        $comment->update($validated);
+        return redirect()->route('admin.comments.create')->with('success', 'Comment has been added');
     }
 
     public function destroy($id)
