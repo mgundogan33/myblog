cecbe7ce24ff800b47423e737e5396d43f87dbee
mgundogan33 <gundogan.mehmet33@gmail.com>
2022-05-17 19:09:01 +0300

    64

--- app/Http/Controllers/AdminControllers/AdminRolesController.php
+++ app/Http/Controllers/AdminControllers/AdminRolesController.php
@@ -2,15 +2,20 @@
 
 namespace App\Http\Controllers\AdminControllers;
 
-use App\Http\Controllers\Controller;
+use App\Models\Role;
 use App\Models\Permission;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class AdminRolesController extends Controller
 {
+    private $rules = ['name' => 'required'];
+
     public function index()
     {
-        //
+        return view('admin_dashboard.roles.edit', [
+            'roles' => Role::all(),
+        ]);
     }
 
     public function create()
@@ -22,26 +27,38 @@ class AdminRolesController extends Controller
 
     public function store(Request $request)
     {
-        //
+        $validated = $request->validate($this->rules);
+        $permissions = $request->input('permissions');
+
+        $role = Role::create($validated);
+        $role->permissions()->sync($permissions);
+
+        return redirect()->route('admin.roles.create')->with('success', 'Role has been created');
     }
 
-    public function show($id)
+    public function edit(Role $role)
     {
-        //
+        return view('admin_dashboard.roles.edit', [
+            'role' => $role,
+            'permissions' => Permission::all()
+        ]);
     }
 
-    public function edit($id)
+    public function update(Request $request, Role $role)
     {
-        //
+        $validated = $request->validate($this->rules);
+        $permissions = $request->input('permissions');
+
+        $role->update($validated);
+        $role->permissions()->sync($permissions);
+
+        return redirect()->route('admin.roles.edit', $role)->with('success', 'Role has been updated');
     }
 
-    public function update(Request $request, $id)
+    public function destroy(Role $role)
     {
-        //
-    }
+        $role->delete();
+        return redirect()->route('admin.roles.index')->with('success', 'Role has been deleted');
 
-    public function destroy($id)
-    {
-        //
     }
 }
